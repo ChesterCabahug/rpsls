e9245b9558b6b10af2aac14bd22836dc90032158
Chester <chesterdeveloper2000@gmail.com>
2020-08-01 11:44:55 +0800

    querySelectors added for every elements.. then added an onclick eventListener in our html

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 
+const playerScoreEl = document.querySelector("#playerScore")
+const playerChoiceEl = document.querySelector("#playerChoice")
+const computerScoreEl = document.querySelector("#computerScore")
+const computerChoiceEl = document.querySelector("#computerChoice")
+const resultText = document.querySelector("#resultText")
+
+
+const playerRock = document.querySelector("playerRock")
+const playerPaper = document.querySelector("playerPaper")
+const playerScissors = document.querySelector("playerScissors")
+const playerLizard = document.querySelector("playerLizard")
+const playerSpock = document.querySelector("playerSpock")
+
+const computerRock = document.querySelector("computerRock")
+const computerPaper = document.querySelector("computerPaper")
+const computerScissors = document.querySelector("computerScissors")
+const computerLizard = document.querySelector("computerLizard")
+const computerSpock = document.querySelector("computerSpock")
+
+const allGameIcons = document.querySelectorAll(".far")
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
